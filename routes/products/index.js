8686e10cb1b114365b3ba53dac767cc946c88d2b
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-10 15:58:15 -0300

    feat: endpoint /:id para a rota de produtos

--- routes/products/index.js
+++ routes/products/index.js
@@ -3,4 +3,6 @@ const productsController = require('../../controllers/productsController');
 
 router.get('/', productsController.getAll);
 
+router.get('/:id', productsController.getProductById);
+
 module.exports = router;
