7dcab67326797d504b9bb0fb010cc315a4ce2a75
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-10 18:56:31 -0300

    feat: retorno de erro no enpoint /:id caso não exista um produto

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,5 +1,5 @@
 module.exports = (err, _req, res, _next) => {
-  if (err.status) return res.status(err.status).json({ message: err.messgae });
+  if (err.status) return res.status(err.status).json({ message: err.message });
 
   return res.status(500).json({ message: 'Internal Server Error' });
 };
--- routes/products/index.js
+++ routes/products/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const rescue = require('express-rescue');
 const productsController = require('../../controllers/productsController');
 
-router.get('/', productsController.getAll);
+router.get('/', rescue(productsController.getAll));
 
-router.get('/:id', productsController.getProductById);
+router.get('/:id', rescue(productsController.getProductById));
 
 module.exports = router;
