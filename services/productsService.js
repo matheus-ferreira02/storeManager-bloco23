20112b831e7e5ff28a5be57104162a2f66c68b10
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-10 15:49:54 -0300

    feat: função getProductById criada

--- services/productsService.js
+++ services/productsService.js
@@ -6,6 +6,13 @@ const getAll = async () => {
   return response;
 };
 
+const getProductById = async (id) => {
+  const [response] = await productsModel.getProductById(id);
+
+  return response;
+};
+
 module.exports = {
   getAll,
+  getProductById,
 };
