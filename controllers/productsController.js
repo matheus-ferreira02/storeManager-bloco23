964a4524ac281f5085a2aa8671e017ef5775075d
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-10 15:56:48 -0300

    feat: função getProductsById na camada controllers criada

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,9 +3,17 @@ const productsService = require('../services/productsService');
 const getAll = async (_req, res) => {
   const response = await productsService.getAll();
 
-  res.status(200).json(response);
+  return res.status(200).json(response);
+};
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  const response = await productsService.getProductById(id);
+
+  return res.status(200).json(response);
 };
 
 module.exports = {
   getAll,
+  getProductById,
 };
