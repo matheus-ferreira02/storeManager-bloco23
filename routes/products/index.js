f7b1483af69f03f17a100f3f47873ad8ac56dc0f
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-11 15:23:53 -0300

    feat: rota de post para products

--- routes/products/index.js
+++ routes/products/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const rescue = require('express-rescue');
 const productsController = require('../../controllers/productsController');
+const validateBody = require('../../middlewares/validateBody');
 
 router.get('/', rescue(productsController.getAll));
 
 router.get('/:id', rescue(productsController.getProductById));
+
+router.post('/', validateBody);
 
 module.exports = router;
